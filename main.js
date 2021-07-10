071483f6a706cd68eb14a0232baec6ca39e37d0c
Simmarsandhu <77791732+Simmarsandhu@users.noreply.github.com>
2021-07-10 15:08:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,131 @@
+canvas=document.getElementById("mycanvas");
+ctx=canvas.getContext("2d");
+
+car_width=100;
+car_height=100;
+
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+car_x=5;
+car_y=225;
+
+
+	function add(){
+
+		background_img= new Image();
+		background_img.onload=uploadBackground;
+		background_img.src=background_image;
+	
+		greencar_img= new Image();
+		greencar_img.onload=uploadgreencar;
+		greencar_img.src=greencar_image;
+	
+	
+		
+	
+	}
+
+
+function uploadBackground() {
+	
+ctx.drawImage(background_img,0,0,canvas.width,canvas.height);
+}
+
+function uploadgreencar() {
+	ctx.drawImage(greencar_img,car_x,car_y,car_width,car_height);
+
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	if(car_y>=0){
+
+		car_y=car_y-10;
+		console.log("x="+car_x+"y="+car_y);
+		uploadBackground();
+		uploadgreencar();
+		check();
+	  }
+}
+
+function down()
+{
+	
+	if(car_y<=300){
+
+		car_y=car_y+10;
+		console.log("x="+car_x+"y="+car_y);
+		uploadBackground();
+		uploadgreencar();
+		check();
+	  }
+}
+
+function left()
+{
+	if(car_x>=0){
+
+		car_x=car_x-10;
+		console.log("x="+car_x+"y="+car_y);
+		uploadBackground();
+		uploadgreencar();
+		check();
+	  }
+}
+
+function right()
+{
+	
+	if(car_x<=700){
+
+		car_x=car_x+10;
+		console.log("x="+car_x+"y="+car_y);
+		uploadBackground();
+		uploadgreencar();
+		check()
+	  }
+	}
+
+
+	function check(){
+		if(car_x=415)+(car_y=85){
+			window.alert("GOOD JOB!")	
+			}
+	}
